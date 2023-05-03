f0cc2fa3a90aa9495f3e3067043c1e52020614f9
Pavel Liashkevich <paul8paul8@mail.ru>
2023-05-03 23:13:21 +0300

    feat: add a separate project card

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -8,12 +8,13 @@ const Projects = () => {
 			<div className='container'>
 				<h2 className='title-1'>Projects</h2>
 				<ul className='projects'>
-					{projects.map(project => {
+					{projects.map((project, index) => {
 						return (
 							<Project
+								key={index}
 								title={project.title}
 								img={project.img}
-								key={project.index}
+								index={index}
 							/>
 						)
 					})}
