97dcd49cca87747f90521fa0f6b0a4229ff68cdd
pavel <paul8paul8@mail.ru>
2024-09-12 10:20:46 +0300

    refactor: skills

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,29 +1,30 @@
-import Header from '../components/Header/Header'
+import Header from "../components/Header/Header"
 
 const Home = () => {
-	return (
-		<>
-			<Header />
-			<main className='section'>
-				<div className='container'>
-					<ul className='content-list'>
-						<li className='content-list__item'>
-							<h2 className='title-2'>Frontend</h2>
-							<p>
-								JavaScript, TypeScript, ReactJS, Redux, Redux Toolkit, REST API,
-								HTML, CSS/S(C|A)SS, NPM, TailwindCSS, StyledComponents,
-								BootStrap, MaterialUI
-							</p>
-						</li>
-						<li className='content-list__item'>
-							<h2 className='title-2'>Backend</h2>
-							<p>NodeJS, MySQL, PHP</p>
-						</li>
-					</ul>
-				</div>
-			</main>
-		</>
-	)
+  return (
+    <>
+      <Header />
+      <main className="section">
+        <div className="container">
+          <ul className="content-list">
+            <li className="content-list__item">
+              <h2 className="title-2">Frontend</h2>
+              <p>
+                JavaScript, TypeScript, React, Redux(RTK, Query), Axios, REST
+                API, TDD, Vite, StoryBook, Git, GitHub, React Router DOM, React
+                Hook Form, Formik, HTML, CSS/S(C|A)SS, Styled Components, Radix
+                UI, Material UI, TailwindCSS, BootStrap
+              </p>
+            </li>
+            <li className="content-list__item">
+              <h2 className="title-2">Backend</h2>
+              <p>NodeJS, MongoDB, PHP</p>
+            </li>
+          </ul>
+        </div>
+      </main>
+    </>
+  )
 }
 
 export default Home
