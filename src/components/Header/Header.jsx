581f8b5d90fe5762496cfbd23a242a5bd49fb0e1
pavel <paul8paul8@mail.ru>
2024-11-12 16:06:33 +0300

    refactor: cv

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,26 +1,26 @@
 import './header.css'
-import cv from '../../helpers/CV-PavelLiashkevich.pdf'
+import cv from '../../helpers/CV-Pavel-Liashkevich.pdf'
 
 const Header = () => {
-	return (
-		<header className='header'>
-			<div className='header__wrapper'>
-				<h1 className='header__title'>
-					<strong>
-						Hi, my name is <em>Pavel</em>
-					</strong>
-					<br />
-					I'm a frontend developer
-				</h1>
-				<div className='header__text'>
-					<p>with passion for learning and creating.</p>
-				</div>
-				<a href={cv} className='btn' download>
-					Download CV
-				</a>
-			</div>
-		</header>
-	)
+    return (
+        <header className='header'>
+            <div className='header__wrapper'>
+                <h1 className='header__title'>
+                    <strong>
+                        Hi, my name is <em>Pavel</em>
+                    </strong>
+                    <br/>
+                    I'm a frontend developer
+                </h1>
+                <div className='header__text'>
+                    <p>with passion for learning and creating.</p>
+                </div>
+                <a href={cv} className='btn' download>
+                    Download CV
+                </a>
+            </div>
+        </header>
+    )
 }
 
 export default Header
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,30 +1,31 @@
 import Header from "../components/Header/Header"
 
 const Home = () => {
-  return (
-    <>
-      <Header />
-      <main className="section">
-        <div className="container">
-          <ul className="content-list">
-            <li className="content-list__item">
-              <h2 className="title-2">Frontend</h2>
-              <p>
-                JavaScript, TypeScript, React, Redux(RTK, Query), Axios, REST
-                API, TDD, Vite, StoryBook, Git, GitHub, React Router DOM, React
-                Hook Form, Formik, HTML, CSS/S(C|A)SS, Styled Components, Radix
-                UI, Material UI, TailwindCSS, BootStrap
-              </p>
-            </li>
-            <li className="content-list__item">
-              <h2 className="title-2">Backend</h2>
-              <p>NodeJS, MongoDB, PHP</p>
-            </li>
-          </ul>
-        </div>
-      </main>
-    </>
-  )
+    return (
+        <>
+            <Header/>
+            <main className="section">
+                <div className="container">
+                    <ul className="content-list">
+                        <li className="content-list__item">
+                            <h2 className="title-2">Frontend</h2>
+                            <p>
+                                TypeScript, JavaScript, Next.js, React(Query), Redux(RTK), RTK Query, Axios, REST API,
+                                TDD, Vite,
+                                StoryBook, Git(GitHub, GitLab), React Router DOM, Formik, React Hook Form, HTML, CSS/S(C|A)SS,
+                                Styled Components,
+                                Radix UI, Material UI, TailwindCSS, BootStrap
+                            </p>
+                        </li>
+                        <li className="content-list__item">
+                            <h2 className="title-2">Backend</h2>
+                            <p>NodeJS, MongoDB, PHP</p>
+                        </li>
+                    </ul>
+                </div>
+            </main>
+        </>
+    )
 }
 
 export default Home
